780e82bc481d69bb72b65a076d0604b684b9da8e
koennecker <koenneckerb@gmail.com>
2019-06-10 11:08:33 -0700

    Add c hello world

---
+++ sg3/intro-to-c/introduction-to-c/day-1/hello_world.c
@@ -0,0 +1,18 @@
+#include <stdio.h> // <-- import input/output from the standard library
+
+// create a main function
+// the compiler looks for the main function
+// in order to execute code
+
+// declare a function called main
+// it returns an integer
+// it takes in no arguments
+int main(void)
+{
+    // this prints Hello world! with a new line to stdout
+    printf("Hello world!\n");
+
+    // we return 0 because nothing went wrong
+    // there are zero errors
+    return 0;
+}
